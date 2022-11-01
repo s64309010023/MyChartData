a36a53ea31b6642c1843fd304f4a27af15c10bec
s64309010023 <s64309010023@ptnicec.ac.th>
2022-11-01 17:29:19 +0700

    ส่งครั้งที่1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {NgChartsModule } from 'ng2-charts'
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
+    NgChartsModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
